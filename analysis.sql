b8431fd20bb5351911c5c5a1678da3895ff483e1
henryfash <44878078+henryfash@users.noreply.github.com>
2023-09-30 23:06:56 +0200

    Added RQ 5,6

--- analysis.sql
+++ analysis.sql
@@ -1,4 +1,6 @@
 
+USE dannys_diner;
+
 -- 1. What is the total amount each customer spent at the restaurant?
 SELECT 
    s.customer_id, SUM(price) 'Total amount spent'
@@ -28,7 +30,7 @@ FROM
 
 -- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
 SELECT 
-    m.product_name, COUNT(*) 'number of purchase'
+    m.product_name, COUNT(*) number_of_purchase
 FROM
     sales s
         JOIN
@@ -38,10 +40,42 @@ ORDER BY 2 DESC
 LIMIT 1;
 
 -- 5. Which item was the most popular for each customer?
-
+WITH subtab AS (SELECT 
+    s.customer_id,
+    m.product_name,
+    COUNT(m.product_name) AS count_name
+FROM
+    sales s
+        JOIN
+    menu m ON s.product_id = m.product_id
+GROUP BY 1 , 2
+ORDER BY 1 , 3 DESC)
+SELECT 
+    customer_id,
+    product_name,
+    MAX(count_name) number_of_purchase
+FROM
+    subtab
+GROUP BY 1;
 
 -- 6. Which item was purchased first by the customer after they became a member?
+
+SELECT 
+    customer_id, product_name
+FROM
+    (SELECT 
+        s.customer_id, m.product_name, MIN(s.order_date)
+    FROM
+        sales s
+    JOIN menu m ON s.product_id = m.product_id
+    JOIN members mb ON s.customer_id = mb.customer_id
+    WHERE
+        s.order_date > mb.join_date
+    GROUP BY 1) subtab;
+
 -- 7. Which item was purchased just before the customer became a member?
+
+    
 -- 8. What is the total items and amount spent for each member before they became a member?
 -- 9.  If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?
 -- 10. In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi - how many points do customer A and B have at the end of January?
