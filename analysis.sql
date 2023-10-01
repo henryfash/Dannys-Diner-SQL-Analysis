01b27b20648ff6e73f5af3f7ab80d30bb6426359
henryfash <44878078+henryfash@users.noreply.github.com>
2023-10-01 15:32:34 +0200

    Add files via upload

--- analysis.sql
+++ analysis.sql
@@ -106,5 +106,64 @@ WHERE ranks = 1
 ORDER BY pm.customer_id ASC;
 
 -- 8. What is the total items and amount spent for each member before they became a member?
+SELECT 
+    s.customer_id,
+    COUNT(s.order_date) total_items,
+    SUM(price) amount_spent
+FROM
+    sales s
+        JOIN
+    menu m ON s.product_id = m.product_id
+        JOIN
+    members mb ON s.customer_id = mb.customer_id
+WHERE
+    s.order_date < mb.join_date
+GROUP BY s.customer_id
+ORDER BY s.customer_id;
+
 -- 9.  If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?
--- 10. In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi - how many points do customer A and B have at the end of January?
+WITH points_gain AS (SELECT 
+    s.customer_id,
+    CASE
+        WHEN s.product_id = 1 THEN price * 10 * 2
+        ELSE price * 10
+    END AS points
+FROM
+    sales s
+        JOIN
+    menu m ON s.product_id = m.product_id
+)
+SELECT 
+    customer_id, SUM(points) total_points
+FROM
+    points_gain
+GROUP BY customer_id;
+
+/*
+-- 10. In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi 
+- how many points do customer A and B have at the end of January? 
+*/
+
+WITH dates_st AS (
+  SELECT 
+    customer_id, 
+    join_date, 
+    DATE_ADD(join_date, INTERVAL 6 DAY) AS valid_date, 
+    DATE_SUB(DATE_ADD(LAST_DAY('2021-01-31'), INTERVAL 1 MONTH), INTERVAL 1 DAY) AS last_date
+  FROM members
+)
+SELECT 
+  s.customer_id, 
+  SUM(CASE
+    WHEN m.product_name = 'sushi' THEN 2 * 10 * m.price
+    WHEN s.order_date BETWEEN d.join_date AND d.valid_date THEN 2 * 10 * m.price
+    ELSE 10 * m.price END) AS total_points
+FROM sales s
+JOIN dates_st d
+  ON s.customer_id = d.customer_id
+  AND d.join_date <= s.order_date
+  AND s.order_date <= d.last_date
+JOIN menu m
+  ON s.product_id = m.product_id
+GROUP BY s.customer_id
+ORDER BY s.customer_id;
